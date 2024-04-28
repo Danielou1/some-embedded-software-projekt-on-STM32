aed2c811c98939cc195685e72fa40ebc4781fdec
Danielle Rosine Ndjensi Felefack <danielle.rosine.ndjensi.felefack@mni.thm.de>
2024-04-28 20:23:21 +0200

    Replace main.c

--- 02_Joystick/src/main.c
+++ 02_Joystick/src/main.c
@@ -1,10 +1,10 @@
 /**
  ******************************************************************************
  * @file    	main.c
- * @author      Danielou Mounsande & Danielle Djentsi
+ * @author      Danielou Mounsande & Danielle Ndjensi
  * @version 	V1.0
- * @date        18.04.2024
- * @brief
+ * @date        24.04.2024
+ * @brief		Vorlage mit dem 8BIT SEG und dem Joystick
  ******************************************************************************
  */
 
@@ -19,30 +19,42 @@ int main(void) {
 
 	//Hardware initialization
 	HAL_Init();
+
+	// Initialisierung des 8-Segment-Displays und des Joysticks
 	esd_init();
 	joystick_init();
 
+	// Initialisierung der Position und des Digits (Zahl) für die Anzeige
 	esd_position_t pos = 0;
 	esd_digit_t digit = 0;
 
 
 	while (1) {
-		if ((utils_gpio_port_read(JOYSTICK_PORT) & JOYSTICK_PIN_UP) == 0) { //JOYSTICK UP
+		uint16_t state = utils_gpio_port_read(JOYSTICK_PORT);
+
+		// Überprüfen, ob die Joystick-Richtungstasten gedrückt werden
+
+		// Joystick UP: Erhöhe das Digit (Zahl) um 1
+		if ((state & JOYSTICK_PIN_UP) == 0) { //JOYSTICK UP
 			digit = (digit + 1) % (10);
 
-		} else if ((utils_gpio_port_read(JOYSTICK_PORT) & JOYSTICK_PIN_DOWN) //JOYSTICK DOWN
+		// Joystick DOWN: Verringere das Digit (Zahl) um 1
+		} else if ((state & JOYSTICK_PIN_DOWN) //JOYSTICK DOWN
 				== 0) {
 			digit = (digit + 9) % (10);
 
-		} else if ((utils_gpio_port_read(JOYSTICK_PORT) & JOYSTICK_PIN_LEFT) // JOYSTICK LEFT
+		// Joystick LEFT: Ändere die Anzeigeposition nach links
+		} else if ((state & JOYSTICK_PIN_LEFT) // JOYSTICK LEFT
 				== 0) {
 			pos = (pos + 4) % (5);
 
-		} else if ((utils_gpio_port_read(JOYSTICK_PORT) & JOYSTICK_PIN_RIGHT) //JOYSTICK RIGHT
+		// Joystick RIGHT: Ändere die Anzeigeposition nach rechts
+		} else if ((state & JOYSTICK_PIN_RIGHT) //JOYSTICK RIGHT
 				== 0) {
 			pos = (pos + 1) % (5);
 
-		} else if ((utils_gpio_port_read(JOYSTICK_PORT) & JOYSTICK_PIN_SELECT) // JOYSTICK PRESS & COUNTDOWN
+			// Joystick SELECT: Starte den Countdown von der aktuellen Zahl
+		} else if ((state & JOYSTICK_PIN_SELECT) // JOYSTICK PRESS & COUNTDOWN
 				== 0) {
 
 			for (int temp = digit; temp >= 0; temp--) {
@@ -53,7 +65,9 @@ int main(void) {
 			}
 		}
 
+		// Zeige das aktuelle Digit (Zahl) an der aktuellen Position auf dem Display
 		esd_show_digit(digit, pos);
+		// Kurze Verzögerung
 		utils_delay_ms(200);
 	}
 }
